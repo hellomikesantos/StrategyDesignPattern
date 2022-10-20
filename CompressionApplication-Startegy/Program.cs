3f4b298a7d3a627f75492578c15fda884836b4bc
Michael John Santos <michaeljohnsantos@student.mitt.ca>
2022-10-20 11:44:52 -0500

    User with Badges Example Decorator Pattern

--- CompressionApplication-Startegy/Program.cs
+++ CompressionApplication-Startegy/Program.cs
@@ -29,6 +29,11 @@ newHondaCar = new LeatherSeats(newHondaCar, "Black Leather Seats");
 newHondaCar = new LeatherSeats(newHondaCar, "Red Leather Seats");
 Console.WriteLine(newHondaCar.GetDetails());
 
+Client newClient = new User("hellomikesantos", "michael@mitt.ca");
+newClient = new Badge1("Pull Shark Badge", 50, newClient);
+newClient = new Badge1("YOLO Badge", 25, newClient);
+Console.WriteLine(newClient.GetDescription());
+Console.WriteLine(newClient.GetReputation());
 
 public abstract class Beverage
 {
@@ -208,3 +213,60 @@ public class HybridEngine : UpgradesDecorator
 // Swagger
 // GET cars/tires
 // add new endpoints that send responses
+
+
+public abstract class Client
+{
+    protected string _userName { get; set; }
+    protected string _email { get; set; }
+    protected string _decription { get; set; } = "";
+    protected int _reputation { get; set; }
+    public virtual int GetReputation()
+    {
+        return _reputation;
+    }
+    public virtual string GetDescription()
+    {
+        return _decription;
+    }
+}
+
+public class User : Client
+{
+    public User(string userName, string email)
+    {
+        _userName = userName;
+        _email = email;
+        _decription = "Base-level User";
+        _reputation = 0;
+    }
+}
+
+public abstract class BadgeDecorator : Client
+{
+    public Client _client { get; set; }
+    public string _description { get; set; }
+    public int _reputation { get; set; }
+    public abstract override int GetReputation();
+    public abstract override string GetDescription();
+}
+
+public class Badge1 : BadgeDecorator
+{
+    public string _description { get; set; }
+    public int _reputation { get; set; }
+    public Badge1(string description, int reputation, Client client)
+    {
+        _reputation = reputation;
+        _description = description;
+        _client = client;
+    }
+    public override string GetDescription()
+    {
+        return $"{_client.GetDescription()} {_description}";
+    }
+    public override int GetReputation()
+    {
+        return _client.GetReputation() + _reputation;
+    }
+}
